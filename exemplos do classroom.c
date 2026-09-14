c88385aab2fb0edfd3b032e7b54ffc297e713077
Lukas Santos <santlucasos778@gmail.com>
2026-09-14 17:42:02 -0400

    Update exemplos do classroom.c

--- exemplos do classroom.c
+++ exemplos do classroom.c
@@ -1,1 +1,27 @@
 
+//CALCULE UM FATORIAL DE UM NUMERO (aguenta ate o 20)
+#include <stdio.h>
+
+int main() {
+
+    int numero;
+    long long fatorial=1,i;
+
+    printf("Entre com o numero que sera fatorado:");
+    scanf("%d", &numero);
+
+    if(numero<=-1){
+
+    printf("\nEste numero nao pode ser fatorado");
+    return 0;
+    }else{
+for (i = 1; i <= numero; i++) {
+        fatorial*=i;
+    }
+    }
+
+
+    printf("\nO fatorial de %d e: %lld\n", numero, fatorial);
+
+    return 0;
+}
