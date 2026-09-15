e3e3af8cfb56ff0fc223c8409f1ec533ca0c23f1
Lukas Santos <santlucasos778@gmail.com>
2026-09-15 14:56:30 -0400

    Update exemplos do classroom.c

--- exemplos do classroom.c
+++ exemplos do classroom.c
@@ -25,3 +25,23 @@ for (i = 1; i <= numero; i++) {
 
     return 0;
 }
+//revisao da prova
+#include <stdio.h>
+int main()
+{
+  int carros_vendidos,valor_vendas,salario_fixo=1200,valorcadacarro=150,salario_total,vendas_porcentagem,valorcadacarroporcetagem;
+
+  printf("Quantos carros voçe vendeu:");
+  scanf("%d",&carros_vendidos);
+
+  printf("qual foi o valor em vendas feitas: ");
+  scanf("%d",&valor_vendas);
+
+  vendas_porcentagem=valor_vendas * 5/100;
+  valorcadacarroporcetagem = carros_vendidos * valorcadacarro;
+  salario_total = salario_fixo + vendas_porcentagem + valorcadacarroporcetagem;
+
+  printf("seu salario total este mês é: %d",salario_total);
+  return 0; 
+
+}
